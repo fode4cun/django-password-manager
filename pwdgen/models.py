1c4af459eb272a34e489fd60522f620c42278532
Anton Dovnar <fode4cun@gmail.com>
2020-12-21 03:38:45 +0300

    Create Category / Repository models
    Repository - password repository with many-to-one
    relation to Category.
    Category - password category.

--- pwdgen/models.py
+++ pwdgen/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from layout.models import CommonFieldsBase, CreationModificationDateBase
+
+
+class Category(CommonFieldsBase, CreationModificationDateBase):
+    slug = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name = ('Category')
+        verbose_name_plural = ('Categories')
+        ordering = ['-created']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+
+class Repository(CommonFieldsBase, CreationModificationDateBase):
+    category = models.ForeignKey(
+        'Category',
+        verbose_name=('Category'),
+        on_delete=models.CASCADE,
+        related_name='categories',
+    )
+    password = models.CharField(('Password'), max_length=255)
+
+    class Meta:
+        verbose_name = ('Repository')
+        verbose_name_plural = ('Repositories')
+        ordering = ['-created']
+
