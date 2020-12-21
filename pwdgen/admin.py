3258381a25b0b30f16ea91475f7d230d42c88ade
Anton Dovnar <fode4cun@gmail.com>
2020-12-21 03:44:58 +0300

    Register models for admin page

--- pwdgen/admin.py
+++ pwdgen/admin.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Repository
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'name', 'created', 'modified', 'slug')
+    list_filter = ('owner', 'created', 'modified')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'owner',
+        'name',
+        'created',
+        'modified',
+        'category',
+        'password',
+    )
+    list_filter = ('owner', 'created', 'modified', 'category')
+    search_fields = ('name',)
