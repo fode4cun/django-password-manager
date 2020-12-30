5ed835e865cd41c95efe2fd8686ce554c6ea6f2c
Anton Dovnar <fode4cun@gmail.com>
2020-12-31 01:44:26 +0300

    Test CategoryFromView / search_icon view

--- pwdgen/tests/test_views.py
+++ pwdgen/tests/test_views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.test import override_settings
+from django.conf import settings
 
 from pwdgen.forms import GeneratorForm
+
+User = get_user_model()
+TEST_MEDIA_PATH = str(settings.BASE_DIR.joinpath('test_media'))
 
 
 class HomeViewTest(TestCase):
@@ -29,4 +35,34 @@ class HomeViewTest(TestCase):
         self.assertRegex(password, r'(?=.*[A-Z])')
         self.assertRegex(password, r'(?=.*[0-9])')
         self.assertEqual(len(password), length)
+
+
+class CategoryFormViewTest(TestCase):
+    url = reverse('pwdgen:category-create')
+
+    @override_settings(MEDIA_ROOT=TEST_MEDIA_PATH)
+    def test_post(self):
+        data = {
+            'name': 'Python',
+            'url': 'https://image.flaticon.com/icons/png/128/3098/3098090.png',
+        }
+        email = 'test@gmail.com'
+        password = 'top_secret'
+
+        user = User.objects.create_user(email=email, password=password)
+        self.client.login(email=email, password=password)
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 302)
+
+
+class SearchIconViewTest(TestCase):
+    url = reverse('pwdgen:search-icon')
+
+    def test_get(self):
+        parameter = {'word': 'python'}
+        response = self.client.get(self.url, parameter)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+        items = response.json()
+        self.assertNotEqual(len(items['results']), 0)
 
