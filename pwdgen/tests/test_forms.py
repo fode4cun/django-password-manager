8f3fba743836c4f8a67211421a17fb483ffdd88f
Anton Dovnar <fode4cun@gmail.com>
2021-01-04 01:18:07 +0300

    Coverage with tests CategoryForm and PasswordForm

--- pwdgen/tests/test_forms.py
+++ pwdgen/tests/test_forms.py
@@ -1,34 +1,60 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings, tag
+from django.urls import reverse
 
-from pwdgen.forms import CategoryForm, GeneratorForm
-
-
-class GeneratorFormTest(TestCase):
-    def test_valid_form(self):
-        data = {
-            'length_range': '15',
-            'length_number': '15',
-            'lowercase': ['on'],
-            'uppercase': ['on'],
-            'numbers': ['on'],
-        }
-        form = GeneratorForm(data=data)
-        self.assertTrue(form.is_valid())
+from pwdgen.tests.mixins import TEST_MEDIA_PATH, SetUpMixin
+from pwdgen.utils import decrypt_password
 
 
-class CategoryFormTest(TestCase):
-    def test_valid_form(self):
+class CategoryFormTest(SetUpMixin, TestCase):
+    url = reverse('pwdgen:category-create')
+
+    @override_settings(MEDIA_ROOT=TEST_MEDIA_PATH)
+    def test_valid_post(self):
         data = {
             'name': 'Python',
             'url': 'https://image.flaticon.com/icons/png/128/3098/3098090.png',
         }
-        form = CategoryForm(data=data)
-        self.assertTrue(form.is_valid())
 
-    def test_invalid_form(self):
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 302)
+
+    @override_settings(MEDIA_ROOT=TEST_MEDIA_PATH)
+    def test_invalid_post(self):
         data = {
-            'name': 'Python',
-            'url': '',
+            'name': 'Web',
+            'url': 'https://image.flaticon.com/icons/png/128/3098/3098090.png',
         }
-        form = CategoryForm(data=data)
-        self.assertEqual(form.errors['url'], ['This field is required.'])
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'name', f'The name {data["name"]} already exists')
+
+
+@tag('pwdform')
+class PasswordFormTest(SetUpMixin, TestCase):
+    url = reverse('pwdgen:password-save')
+
+    def test_valid_post(self):
+        decrypted_pwd = decrypt_password(self.crypted_pwd)
+        data = {
+            'category': str(self.category.pk),
+            'name': 'Python',
+            'password': 'strong_password'
+        }
+        response = self.client.post(self.url, data)
+
+        self.assertNotEqual(self.crypted_pwd, 'strong_password')
+        self.assertEqual(decrypted_pwd, 'strong_password')
+        self.assertEqual(response.status_code, 302)
+
+    def test_invalid_post(self):
+        data = {
+            'category': str(self.category.pk),
+            'name': 'Gmail',
+            'password': 'strong_password'
+        }
+        response = self.client.post(self.url, data)
+        error = response.json()['name']
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(error, [f'The name {data["name"]} already exists'])
