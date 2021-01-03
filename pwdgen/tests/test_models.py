212da098930915f76d016a5a5dba4686977e19bf
Anton Dovnar <fode4cun@gmail.com>
2021-01-04 01:16:43 +0300

    Coverage models with tests

--- pwdgen/tests/test_models.py
+++ pwdgen/tests/test_models.py
@@ -1,21 +1,21 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils.text import slugify
 
-from pwdgen.models import Category
+from pwdgen.models import Category, Password
+from pwdgen.tests.mixins import SetUpMixin
 
-User = get_user_model()
 
-
-class CategoryTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(
-            email='jacob@gmail.com', password='top_secret')
-        self.category = Category.objects.create(
-            owner=self.user, name='Web Pages')
-
+class CategoryTest(SetUpMixin, TestCase):
     def test_category_creation(self):
         category = self.category
+
         self.assertIsInstance(category, Category)
         self.assertEqual(category.slug, slugify(category.name))
         self.assertEqual(category.__str__(), category.name)
+
+    def test_password_creation(self):
+        password = self.password
+
+        self.assertIsInstance(password, Password)
+        self.assertEqual(password.slug, slugify(password.name))
+        self.assertEqual(password.__str__(), password.name)
